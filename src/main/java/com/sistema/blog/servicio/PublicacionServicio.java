7ab5485432a6732ca4c2808a558895dda0aceb59
JOBFER1 <jobfer@outlook.es>
2023-03-04 00:27:29 +0100

    Crea el método GET (listar publicaciones)
    Para listar todas las publicaciones:
    - Método "listarPublicaciones" en el controlador
    - Método "obtenerTodasLasPublicaciones" en el servicio
    - Método "obtenerTodasLasPublicaciones" en la implementación
    - Métodos "mapearDTO" y "mapearEntidad" para refractorizar

--- src/main/java/com/sistema/blog/servicio/PublicacionServicio.java
+++ src/main/java/com/sistema/blog/servicio/PublicacionServicio.java
@@ -1,9 +1,13 @@
 package com.sistema.blog.servicio;
 
+import java.util.List;
+
 import com.sistema.blog.dto.PublicacionDTO;
 
 public interface PublicacionServicio {
 
     public PublicacionDTO crearPublicacion(PublicacionDTO publicacionDTO);
+    
+    public List<PublicacionDTO> obtenerTodasLasPublicaciones();
 	
 }
--- src/main/java/com/sistema/blog/servicio/PublicacionServicioImpl.java
+++ src/main/java/com/sistema/blog/servicio/PublicacionServicioImpl.java
@@ -1,5 +1,8 @@
 package com.sistema.blog.servicio;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,20 +19,40 @@ public class PublicacionServicioImpl implements PublicacionServicio {
 	@Override
 	public PublicacionDTO crearPublicacion(PublicacionDTO publicacionDTO) {
 		//Convertimos de DTO a entidad
-		Publicacion publicacion = new Publicacion();
-		publicacion.setTitulo(publicacionDTO.getTitulo());
-		publicacion.setDescripcion(publicacionDTO.getDescripcion());
-		publicacion.setContenido(publicacionDTO.getContenido());
+		Publicacion publicacion = mapearEntidad(publicacionDTO);
 
 		Publicacion nuevaPublicacion = publicacionRepositorio.save(publicacion);
 
 		//Convertimos entidad a DTO
-		PublicacionDTO publicacionRespuesta = new PublicacionDTO();
-		publicacionRespuesta.setId(nuevaPublicacion.getId());
-		publicacionRespuesta.setTitulo(nuevaPublicacion.getTitulo());
-		publicacionRespuesta.setDescripcion(nuevaPublicacion.getDescripcion());
-		publicacionRespuesta.setContenido(nuevaPublicacion.getContenido());
-		
+		PublicacionDTO publicacionRespuesta = mapearDTO(nuevaPublicacion);
 		return publicacionRespuesta;
+	}
+
+	@Override
+	public List<PublicacionDTO> obtenerTodasLasPublicaciones() {
+		List<Publicacion> publicaciones = publicacionRepositorio.findAll();
+		return publicaciones.stream().map(publicacion -> mapearDTO(publicacion)).collect(Collectors.toList());
+	}
+	
+	// Convierte entidad a DTO
+	private PublicacionDTO mapearDTO(Publicacion publicacion) {
+		
+		PublicacionDTO publicacionDTO = new PublicacionDTO();
+		publicacionDTO.setId(publicacion.getId());
+		publicacionDTO.setTitulo(publicacion.getTitulo());
+		publicacionDTO.setDescripcion(publicacion.getDescripcion());
+		publicacionDTO.setContenido(publicacion.getContenido());
+		
+		return publicacionDTO;
+	}
+
+	// Convierte de DTO a Entidad
+	private Publicacion mapearEntidad(PublicacionDTO publicacionDTO) {
+		
+		Publicacion publicacion = new Publicacion();
+		publicacion.setTitulo(publicacionDTO.getTitulo());
+		publicacion.setDescripcion(publicacionDTO.getDescripcion());
+		publicacion.setContenido(publicacionDTO.getContenido());
+		return publicacion;
 	}
 }
